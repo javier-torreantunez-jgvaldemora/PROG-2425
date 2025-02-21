245fa372fc679c0875eff72e0a25d07a13c57959
Javier Torre Antúnez <javier.torreantunez@jgvaldemora.org>
2025-02-21 09:39:49 +0100

    A2 - realizada

--- u07/CuentaCorriente.java
+++ u07/CuentaCorriente.java
@@ -1,20 +1,14 @@
-package u07_a01;
+package u07_a02_b;
 
 class CuentaCorriente {
    String dni; 
    String nombre; 
    double saldo; 
-
-   CuentaCorriente(String dni, String nombre) { 
-      this.dni = dni; 
-      this.nombre = nombre; 
-      saldo = 0;  
-   }
-
-   boolean retirar(double cant) { 
+   
+   boolean egreso(double cant) { 
       boolean operacionPosible;
       if (saldo >= cant) { 
-         saldo -= cant;     
+         saldo -= cant;
          operacionPosible = true;
       } else { 
          System.out.println("No hay dinero suficiente");
@@ -23,13 +17,27 @@ class CuentaCorriente {
       return (operacionPosible); 
    }
 
-   void ingresar(double cant) { 
+   void ingreso(double cant) { 
       saldo += cant;
    }
 
-   void mostrarInformacion() {  
+   void mostrarInformacion() { 
       System.out.println("Nombre: " + nombre);
       System.out.println("Dni: " + dni);
       System.out.println("Saldo: " + saldo + " euros");
+   }
+
+   CuentaCorriente(String dni, String nombre) { 
+      this(dni, nombre, 0); 
+   }
+
+   CuentaCorriente(String dni, double saldo) {
+      this(dni, "Sin asignar", saldo); 
+   }
+   
+   CuentaCorriente(String dni, String nombre, double saldo) {
+      this.dni = dni;
+      this.nombre = nombre;
+      this.saldo = saldo;
    }
 }
